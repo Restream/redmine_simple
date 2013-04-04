0e8e56b8a64c9206c62ec48b88095f946a7b7039
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-04-04 14:02:10 +0400

    simplify assignee field on update issue form; search in groups

--- lib/redmine_simple/hooks/view_hooks.rb
+++ lib/redmine_simple/hooks/view_hooks.rb
@@ -2,5 +2,8 @@ module RedmineSimple::Hooks
   class ViewHooks < Redmine::Hook::ViewListener
     render_on :view_issues_new_top,
               :partial => 'hooks/redmine_simple/issues/new'
+
+    render_on :view_issues_form_details_bottom,
+              :partial => 'hooks/redmine_simple/issues/assignee'
   end
 end
