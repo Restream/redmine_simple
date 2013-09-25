0f5b84feaedd1ea9c737f3f298d829fe28a1dcda
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-09-25 23:03:18 +0400

    block page while updating issue form through ajax

---
+++ assets/javascripts/simplify_block_ui.js
@@ -0,0 +1,28 @@
+/* Block page while updating issue form */
+
+$.blockUI.defaults.message = '';
+$.blockUI.defaults.overlayCSS = '0';
+
+var issueFormBlocker = {
+  enabled: false,
+  enable:  function() { this.enabled = true; },
+  disable: function() { this.enabled = false; },
+  block:   function() {
+    if (this.enabled) { $.blockUI(); }
+  }
+};
+
+$(document).ajaxStart(function() {
+  issueFormBlocker.block();
+});
+
+$(document).ajaxStop(function() {
+  issueFormBlocker.disable();
+  $.unblockUI();
+});
+
+var origUpdateIssueFrom = updateIssueFrom;
+updateIssueFrom = function(url) {
+  issueFormBlocker.enable();
+  origUpdateIssueFrom(url);
+};
