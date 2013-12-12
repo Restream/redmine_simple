11e30a3881f77c4c93b8b9f62a1ec45832c7657a
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-12-12 14:32:57 +0400

    adaptive height for description

---
+++ assets/javascripts/simplify_description.js
@@ -0,0 +1,17 @@
+function adaptiveHeight(a, min, max) {
+  if ($(a).height() > min) {
+    $(a).height(0);
+    var newHeight = Math.min($(a)[0].scrollHeight, max);
+    $(a).height(newHeight);
+    if (parseInt(a.style.height) > $(window).height() - 30) {
+      $(document).scrollTop(parseInt(a.style.height));
+    }
+  }
+}
+
+$(document).ready(function() {
+  var text_areas = '#issue_description,#issue_notes';
+  $(text_areas).each(function() {
+    adaptiveHeight(this, 150, 400);
+  })
+});
